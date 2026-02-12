cc3595ead2eea92e4d763874ee2fd32c30ec0ca5
dalcocer-141103 <dalcocer@eritech.com.mx>
2026-02-12 12:07:22 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/698bc38a55a5d71c35acc4ee/1jh7cqi06';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-
 const URL_JSON = "https://raw.githubusercontent.com/dalcocer-141103/Catalogo-productos/refs/heads/main/productos.json";
 const TELEFONO = "525515107577"; 
 const ITEMS_POR_PAGINA = 12; 
